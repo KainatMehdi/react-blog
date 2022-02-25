43f3b77892b03c597a3ce7ceee1f1057e624f57e
Kainat Mehdi <akhundmehdi@outlook.com>
2022-02-26 01:14:45 +0500

    Sixth Commit

--- api/index.js
+++ api/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -12,6 +14,8 @@ mongoose
   })
   .then(console.log("Connected to MongoDB"))
   .catch(console.log);
+
+app.use("/api/auth", authRoute);
 
 app.listen("3000", () => {
   console.log("Backend is running");
